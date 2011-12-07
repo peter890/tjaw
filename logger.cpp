f9f7a172a0a558356af7cd98899fdc40cc4e74bb
Peter890 <zolter17@gmail.com>
2011-12-07 12:24:31 +0100

    Dodano klase Logger. Klasa reprezentuje wzorzec projektowy Singleton.

---
+++ logger.cpp
@@ -0,0 +1,31 @@
+#include "logger.h"
+#include "QDateTime"
+bool Logger::exists = 0;
+Logger* Logger::instance = 0;
+Logger::Logger()
+{
+    this->exists = 1;
+    QDateTime dt;
+    fileName = "log_";
+    fileName.append(dt.currentDateTime().toString("yyyy_MM_dd__hh_mm_ss"));
+    fileName.append(".log");
+    loggFile = new QFile(fileName);
+    loggFile->open(QIODevice::WriteOnly | QIODevice::Text);
+    //textStream = new QTextStream(loggFile);
+}
+
+Logger* Logger::getInstance()
+{
+    if(Logger::exists)
+            return instance;
+        else
+        {
+            instance = new Logger();
+            return instance;
+        }
+}
+void Logger::logguj(QString s)
+{
+    QTextStream stream(loggFile);
+    stream << s << endl;
+}
---
+++ logger.h
@@ -0,0 +1,20 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+#include <QFile>
+#include <QTextStream>
+class Logger
+{
+private:
+    Logger();
+    Logger(const Logger &);
+    static bool exists;
+    static Logger* instance;
+    QFile* loggFile;
+    QTextStream* textStream;
+    QString fileName;
+public:
+    static Logger* getInstance();
+    void logguj(QString);
+};
+
+#endif // LOGGER_H
